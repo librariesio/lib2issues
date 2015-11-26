feb30ea65ce47c40f136aa308d3f5ec982973d33
Andrew Nesbitt <andrewnez@gmail.com>
2015-11-26 08:17:05 +0000

    Add option to skip prereleases
    Closes #3

--- app.rb
+++ app.rb
@@ -20,13 +20,27 @@ class Lib2Issue < Sinatra::Base
   post '/webhook' do
     content_type :json
     puts @request_payload.inspect
-    client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
-    client.create_issue(@request_payload['repository'], "Upgrade #{@request_payload['name']} to version #{@request_payload['version']}",
-    "Libraries.io has found that there is a newer version of #{@request_payload['name']} that this project depends on.
 
-    More info: https://libraries.io/#{@request_payload['platform'].downcase}/#{@request_payload['name']}/#{@request_payload['version']}",
-    labels: ENV['GITHUB_LABELS'])
+    begin
+      version = SemanticRange.valid(@request_payload['version'])
+    rescue
+      version = @request_payload['version']
+    end
+
+    unless ENV['SKIP_PRERELEASE'] && version.is_a?(SemanticRange::Version) && version.prerelease.present?
+      create_issue(@request_payload['repository'], @request_payload['platform'], @request_payload['name'], @request_payload['version'])
+    end
+
     status 200
     body ''
+  end
+
+  def create_issue(repository, platform, name, version)
+    client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+    client.create_issue(repository, "Upgrade #{name} to version #{version}",
+    "Libraries.io has found that there is a newer version of #{name} that this project depends on.
+
+    More info: https://libraries.io/#{platform.downcase}/#{name}/#{version}",
+    labels: ENV['GITHUB_LABELS'])
   end
 end
